28d54a6a4b2071638c61c45eadbe7e5c26cc81eb
Caio César Silva <caiocesarsilva08@gmail.com>
2022-03-24 19:14:56 -0300

    Adding more fields

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,12 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "horse", "lizard"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("Belo Horizonte, MG");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
 
   return (
     <div className="search-params">
@@ -15,6 +20,34 @@ const SearchParams = () => {
             placeholder="Location"
           />
         </label>
+        <label htmlFor="animal">Animal</label>
+        <select
+          id="animal"
+          value={animal}
+          onChange={(e) => setAnimal(e.target.value)}
+          onBlur={(e) => setAnimal(e.target.value)}
+        >
+          <option value=""></option>
+          {ANIMALS.map((animal) => (
+            <option value={animal} key={animal}>
+              {animal}
+            </option>
+          ))}
+        </select>
+        <label htmlFor="breed">Breed</label>
+        <select
+          id="breed"
+          value={breed}
+          onChange={(e) => setBreed(e.target.value)}
+          onBlur={(e) => setBreed(e.target.value)}
+        >
+          <option value=""></option>
+          {breeds.map((breed) => (
+            <option value={breed} key={breed}>
+              {breed}
+            </option>
+          ))}
+        </select>
         <button>Submit</button>
       </form>
     </div>
