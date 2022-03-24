446bae738704fc448c847008ca228e54dacb98ac
Caio César Silva <caiocesarsilva08@gmail.com>
2022-03-24 17:56:23 -0300

    ESLint and React configuration

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "my-brand" }, "Adopt me!"),
-    React.createElement(Pet, {
-      name: "Charlie",
-      animal: "Dog",
-      breed: "Shih tzu",
-    }),
-    React.createElement(Pet, {
-      name: "Kyubi",
-      animal: "Cat",
-      breed: "Unknown",
-    }),
-    React.createElement(Pet, {
-      name: "Tod",
-      animal: "Dog",
-      breed: "Lhasa Apso",
-    }),
-  ]);
+  return (
+    <div>
+      <h1 id="my-brand">Adopt me!</h1>
+      <Pet name="Charlie" animal="Dog" breed="Shih tzu" />
+      <Pet name="Kyubi" animal="Cat" breed="Unknown" />
+      <Pet name="Tod" animal="Dog" breed="Lhasa Apso" />
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
