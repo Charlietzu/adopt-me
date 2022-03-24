d45217b562fc2d68496b1f8f384a9b8f930b2f5a
Caio César Silva <caiocesarsilva08@gmail.com>
2022-03-24 16:15:49 -0300

    Pet component refactoring

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-const Pet = () => {
+const Pet = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h2", {}, "Charlie"),
-    React.createElement("h3", {}, "Dog"),
-    React.createElement("h3", {}, "Shih tzu"),
+    React.createElement("h2", {}, props.name),
+    React.createElement("h3", {}, props.animal),
+    React.createElement("h3", {}, props.breed),
   ]);
 };
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", { id: "my-brand" }, "Adopt me!"),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet),
+    React.createElement(Pet, {
+      name: "Charlie",
+      animal: "Dog",
+      breed: "Shih tzu",
+    }),
+    React.createElement(Pet, {
+      name: "Kyubi",
+      animal: "Cat",
+      breed: "Unknown",
+    }),
+    React.createElement(Pet, {
+      name: "Tod",
+      animal: "Dog",
+      breed: "Lhasa Apso",
+    }),
   ]);
 };
 
