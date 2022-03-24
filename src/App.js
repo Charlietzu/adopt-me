246a7ee597991364027f0bf4d27b387c167ffd77
Caio César Silva <caiocesarsilva08@gmail.com>
2022-03-24 18:34:49 -0300

    ESLint hooks configuration

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1 id="my-brand">Adopt me!</h1>
-      <Pet name="Charlie" animal="Dog" breed="Shih tzu" />
-      <Pet name="Kyubi" animal="Cat" breed="Unknown" />
-      <Pet name="Tod" animal="Dog" breed="Lhasa Apso" />
+      <SearchParams />
     </div>
   );
 };
