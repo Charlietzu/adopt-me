267dfa7ed2d55991590a76f5a0831f2fe66c56ce
Caio César Silva <caiocesarsilva08@gmail.com>
2022-03-25 13:41:58 -0300

    Routing configuration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      <h1 id="my-brand">Adopt me!</h1>
-      <SearchParams />
+      <BrowserRouter>
+        <header>
+          <Link to="/">
+            <h1 id="my-brand">Adopt me!</h1>
+          </Link>
+        </header>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 };
