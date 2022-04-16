221ccdad243c00db80ae688ed60860db42baa228
Caio César Silva <caiocesarsilva08@gmail.com>
2022-04-16 12:56:35 -0300

    Tailwind positioning

--- src/Pet.js
+++ src/Pet.js
@@ -6,12 +6,12 @@ const Pet = ({ name, animal, breed, images, location, id }) => {
     hero = images[0];
   }
   return (
-    <Link to={`/details/${id}`}>
+    <Link to={`/details/${id}`} className="relative block">
       <div>
         <img src={hero} alt={name} />
       </div>
 
-      <div>
+      <div className="absolute bottom-0 left-0 bg-gradient-to-tr from-white to-transparent pr-2">
         <h1>{name}</h1>
         <h2>{`${animal} - ${breed} - ${location}`}</h2>
       </div>
