f52f79807b66cebf13beda3b7ff00b0ae5fa736a
Caio César Silva <caiocesarsilva08@gmail.com>
2022-04-16 13:30:14 -0300

    Code splitting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { StrictMode, useState } from "react";
+import { StrictMode, useState, lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
-import SearchParams from "./SearchParams";
-import Details from "./Details";
 import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import ThemeContext from "./ThemeContext";
+
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 
 const App = () => {
   const themeHook = useState("darkblue");
@@ -16,24 +17,26 @@ const App = () => {
             "url(http://pets-images.dev-apis.com/pets/wallpaperA.jpg)",
         }}
       >
-        <BrowserRouter>
-          <header
-            className="w-full mb-10 text-center p-7 bg-gradient-to-b
+        <Suspense fallback={<h2>loading route...</h2>}>
+          <BrowserRouter>
+            <header
+              className="w-full mb-10 text-center p-7 bg-gradient-to-b
             from-purple-400 via-pink-500 to-red-500"
-          >
-            <Link to="/" className="text-6xl text-white hover:text-gray-200">
-              <h1 id="my-brand">Adopt me!</h1>
-            </Link>
-          </header>
-          <Switch>
-            <Route path="/details/:id">
-              <Details />
-            </Route>
-            <Route path="/">
-              <SearchParams />
-            </Route>
-          </Switch>
-        </BrowserRouter>
+            >
+              <Link to="/" className="text-6xl text-white hover:text-gray-200">
+                <h1 id="my-brand">Adopt me!</h1>
+              </Link>
+            </header>
+            <Switch>
+              <Route path="/details/:id">
+                <Details />
+              </Route>
+              <Route path="/">
+                <SearchParams />
+              </Route>
+            </Switch>
+          </BrowserRouter>
+        </Suspense>
       </div>
     </ThemeContext.Provider>
   );
